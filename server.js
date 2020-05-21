6cc66bfa962ea547e17b76e0c6c715a121570bb0
Donghyeon Kim <0916dhkim@gmail.com>
2020-05-21 11:42:50 +0900

    Implement Endpoint

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+const multer = require("multer");
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(multer().single("upfile"));
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
@@ -16,6 +18,15 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post("/api/fileanalyse", function(req, res) {
+  console.log(req.file);
+  res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
